5f65b51a9177f14d51e1d9d54f8117dbea7382f9
Albert <krm9346@autuni.ac.nz>
2024-12-25 09:48:18 +0700

    update app.component to router.navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-router-sample';
+
+  constructor(private router: Router) {}
+
+  navigateToCrisisCenter(): void {
+    this.router.navigate(['/crisis-list']);
+  }
+
+  navigateToHeroes(): void {
+    this.router.navigate(['/heroes-list']);
+  }
 }

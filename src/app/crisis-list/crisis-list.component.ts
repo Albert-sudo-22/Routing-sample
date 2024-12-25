0009133dfc54e6815de8324360dc66e67fbd4058
Albert <krm9346@autuni.ac.nz>
2024-12-25 15:08:33 +0700

    update fetch api

--- src/app/crisis-list/crisis-list.component.ts
+++ src/app/crisis-list/crisis-list.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.services';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-crisis-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './crisis-list.component.html',
   styleUrl: './crisis-list.component.css'
 })
-export class CrisisListComponent {
+export class CrisisListComponent implements OnInit {
+  users: any[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.fetchUsers().subscribe(
+      (data) => {
+        this.users = data.users;
+      },
+      (error) => {
+        console.error('Error fetching users', error);
+      }
+    );
+  }
 }
